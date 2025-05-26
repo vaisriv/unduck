53ad270b31ae56720e58e1051a5f42fabe6f537c
vai <vai.sriv@icloud.com>
2025-05-25 19:58:55 -0700

    add consistency with /

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ function noSearchDefaultPageRender() {
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.astrophel.xyz?q=%s"
+            value="https://unduck.astrophel.xyz/?q=%s"
             readonly 
           />
           <button class="copy-button">
